a947699d88a5e63da99acba4291633d0f2f26d1e
Joseph C Wang <joequant@gmail.com>
2025-12-22 19:52:31 +0800

    add scripts

---
+++ scripts/deploy-data-oracle.ts
@@ -0,0 +1,16 @@
+import hre from "hardhat";
+const { viem, networkHelpers } = await hre.network.connect();
+
+async function main() {
+  const token = await viem.deployContract(
+     "DataOracle"
+  );
+  console.log("Deployed contract address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/initialize-testnet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR/..
+
+# This is a test environment that uses the hardhat local accounts
+# Define a function to clean up and kill all children
+cleanup_and_exit() {
+    echo "Interrupted. Killing all child processes."
+    pgrep -P $$ | xargs kill
+}
+
+(npx hardhat node) &
+
+echo "node started waiting...."
+sleep 3
+
+npx hardhat --network localhost run scripts/deploy-mock-erc20.ts
+npx hardhat --network localhost run scripts/deploy-data-oracle.ts
+npx hardhat --network localhost test
+
+trap "cleanup_and_exit" INT  # Ctrl+C
+sleep infinity
+
