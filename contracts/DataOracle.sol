7d5b073b6c91bf2beed5b54a7fe6fb171d7e175a
Joseph C Wang <joequant@gmail.com>
2025-12-22 19:52:38 +0800

    add natspec

--- contracts/DataOracle.sol
+++ contracts/DataOracle.sol
@@ -6,14 +6,28 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 error IndexOutOfBounds();
 
+/**
+ * @title DataOracle
+ * @dev A contract to store and retrieve timestamped data with historical records.
+ */
+
 contract DataOracle is Ownable, AccessControl {
     // Role for data updater
     bytes32 public constant DATA_UPDATER_ROLE = keccak256("DATA_UPDATER_ROLE");
 
-    // Event triggered when data is updated
+    /**
+     * @dev Emitted when data is updated.
+     * @param timestamp The timestamp of the update.
+     * @param data The data that was updated.
+     * @param historicalCount The total count of historical records after the update.
+     */
     event DataUpdated(uint256 timestamp, uint256 data, uint256 historicalCount);
 
-    // Struct to hold timestamped data
+    /**
+     * @dev Struct to hold timestamped data.
+     * @param timestamp The timestamp of the data.
+     * @param data The data value.
+     */
     struct TimestampedData {
         uint256 timestamp;
         uint256 data;
@@ -26,13 +40,20 @@ contract DataOracle is Ownable, AccessControl {
     mapping(uint256 => TimestampedData) private historicalData;
     uint256 private historicalCount;
 
-    // Constructor
+    /**
+     * @dev Constructor to initialize the contract.
+     * Grants the deployer the DEFAULT_ADMIN_ROLE and DATA_UPDATER_ROLE.
+     */
     constructor() Ownable(msg.sender) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(DATA_UPDATER_ROLE, msg.sender);
     }
 
-    // Function for admin to set timestamped data
+    /**
+     * @dev Sets the timestamped data and stores it as a historical record.
+     * Only users with the DATA_UPDATER_ROLE can call this function.
+     * @param _data The data to be set.
+     */
     function setData(uint256 _data) public onlyRole(DATA_UPDATER_ROLE) {
         // Update current data
         lastData = TimestampedData({
@@ -47,18 +68,30 @@ contract DataOracle is Ownable, AccessControl {
         emit DataUpdated(block.timestamp, _data, historicalCount);
     }
 
-    // Function for users to get the last updated data
+    /**
+     * @dev Returns the last updated data.
+     * @return timestamp The timestamp of the last update.
+     * @return data The data value of the last update.
+     */
     function getLastUpdate() public view returns (uint256, uint256) {
         return (lastData.timestamp, lastData.data);
     }
 
-    // Function to get historical data by index
+    /**
+     * @dev Returns historical data by index.
+     * @param index The index of the historical record to retrieve.
+     * @return timestamp The timestamp of the historical record.
+     * @return data The data value of the historical record.
+     */
     function getHistoricalData(uint256 index) public view returns (uint256, uint256) {
         require(index < historicalCount, IndexOutOfBounds());
         return (historicalData[index].timestamp, historicalData[index].data);
     }
 
-    // Function to get total historical records count
+    /**
+     * @dev Returns the total number of historical records.
+     * @return The count of historical records.
+     */
     function getHistoricalCount() public view returns (uint256) {
         return historicalCount;
     }
