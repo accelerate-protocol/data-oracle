e494653728d778e205dcb060c28cba1eccdff062
Joseph C Wang <joequant@gmail.com>
2025-12-22 18:23:47 +0800

    add user and mark Event as broken

--- test-broken/Event.ts
+++ test-broken/Event.ts
@@ -1,4 +1,8 @@
 // test/DataOracle.Historical.test.js
+// Disable:  There seems to be an issue with viem.assertions not
+// sending the write accounts when used with a local blockchain
+
+
 import { before, describe, it } from "node:test";
 import { assert, expect } from 'chai';
 import hre from "hardhat";
@@ -26,9 +30,13 @@ describe('DataOracle - Events', () => {
   it("should emit DataUpdated event when setData is called", async function () {
     const testData = 12345;
     // Deploy contract
-    const dataOracle = await viem.deployContract('DataOracle', []);
+    const dataOracle = await viem.deployContract('DataOracle', [], {
+      client: owner
+    });
     await viem.assertions.emitWithArgs(
-      dataOracle.write.setData([testData]),
+      dataOracle.write.setData([testData], {
+        account: owner.account.address
+      }),
       dataOracle,
       "DataUpdated",
       [anyValue, 12345n, 1n]
@@ -38,16 +46,24 @@ describe('DataOracle - Events', () => {
   it("should emit DataUpdated event with correct historical count", async function () {
     const contract = await viem.deployContract('DataOracle', []);
     // Set first data
-    await contract.write.setData([100]);
+    await contract.write.setData([100], {
+        account: owner.account.address
+      });
     
     // Set second data
-    await contract.write.setData([200]);
+    await contract.write.setData([200], {
+        account: owner.account.address
+      });
     
     // Set third data
-    await contract.write.setData([300]);
+    await contract.write.setData([300], {
+        account: owner.account.address
+      });
 
     await viem.assertions.emitWithArgs(
-      await contract.write.setData([400]),
+      await contract.write.setData([400],{
+        account: owner.account.address
+      }),
       contract,
       "DataUpdated",
       [anyValue, 400n, 4n]
